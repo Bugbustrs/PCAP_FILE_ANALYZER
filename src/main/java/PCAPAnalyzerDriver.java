e0c7eff510d64507a959e4beb55054bc6bba491e
MelulekiDube <mellarmellah@gmail.com>
2019-08-19 12:41:17 +0200

    Init dev (#6)
    * Updated .gitignore
    
    * Basic implementation done now. we are able to get files from a server and then parse the files.
    
    * Update .gitignore
    
    * Update .gitignore
    
    * initial impl of file monitor
    
    * Updated gitignore
    
    * Basic implementation is finished for this to work
    
    * initial impl of file monitor (#2)
    
    * initial impl of file monitor
    
    * Updated gitignore
    
    * Basic implementation is finished for this to work
    
    * Removed the logic for changing file events
    
    * Cleared the lists after sending them to orchastrator
    
    * removed intelj file
    
    * Included the pcap analyzer to the list of modules.
    All configurations now written to one config and each module on start will take in its configurations
    
    * Removed the PcapMeasurement class from here.
    
    * Delete pcapanalyzer.iml

--- src/main/java/PCAPAnalyzerDriver.java
+++ src/main/java/PCAPAnalyzerDriver.java
@@ -1,3 +1,6 @@
+
+import org.json.JSONObject;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -5,22 +8,27 @@ import java.util.concurrent.TimeUnit;
 public class PCAPAnalyzerDriver {
     private FileMonitor fileMonitor;
     private final ScheduledExecutorService scheduler;
-
-    public PCAPAnalyzerDriver() {
-        fileMonitor = new FileMonitor(Config.HOST_NAME);
+    static JSONObject CONFIG;
+    public PCAPAnalyzerDriver(JSONObject config) {
+        CONFIG =config;
+        fileMonitor = new FileMonitor(config.getString("FILE_SERVER_HOSTNAME"));
         scheduler = Executors.newScheduledThreadPool(1);
     }
 
     /**
      * This methods starts the file monitor and also creates the Executor services that will be running running the analyzer after a certain time.
      */
-    public void initiate() {
+    public boolean initiate() {
 
         if (fileMonitor.start()) {
             /*we want to create the scheduled executor service*/
-            scheduler.scheduleAtFixedRate(new Analyzer(fileMonitor), Config.MINUTES_INIT_DELAY, Config.MINUTES_TILL_ANALYZE_PCAPFILES, TimeUnit.MINUTES);
+            int init_delay = CONFIG.getInt("MINUTES_INIT_DELAY");
+            int minutes_till_analyze = CONFIG.getInt("MINUTES_TILL_ANALYZE_PCAPFILES");
+            scheduler.scheduleAtFixedRate(new Analyzer(fileMonitor), init_delay, minutes_till_analyze, TimeUnit.MINUTES);
+            return true;
         } else {//if we cant connect
             System.err.println("Please check the host in the config file and try to correct that and then restart this service.");
+            return false;
         }
     }
 }
